0c60f8351eca216d008f641cbd6d15428f57dbed
bala-amypo <bala@amypo.in>
2025-12-19 16:01:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/Entities/Certificate.java
@@ -0,0 +1,38 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "certificates")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Certificate 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "template_id")
+    private CertificateTemplate template;
+
+    private LocalDate issuedDate;
+
+    @Column(unique = true)
+    private String verificationCode;
+
+    private String qrCodeUrl;
+
+    @OneToMany(mappedBy = "certificate", cascade = CascadeType.ALL)
+    private List<VerificationLog> logs;
+}
---
+++ src/main/java/com/example/demo/Entities/CertificateTemplate.java
@@ -0,0 +1,29 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "certificate_templates")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CertificateTemplate 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String templateName;
+
+    private String backgroundUrl;
+    private String fontStyle;
+    private String signatureName;
+
+    @OneToMany(mappedBy = "template", cascade = CascadeType.ALL)
+    private List<Certificate> certificates;
+}
