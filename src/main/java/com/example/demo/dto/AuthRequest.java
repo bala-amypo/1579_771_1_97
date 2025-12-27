ead10f287c52e0d89edad32508b460151404f501
bala-amypo <bala@amypo.in>
2025-12-27 21:27:39 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -4,6 +4,11 @@ public class AuthRequest {
     private String email;
     private String password;
 
+    // Getters
     public String getEmail() { return email; }
     public String getPassword() { return password; }
-    public void
+
+    // Setters
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+}
